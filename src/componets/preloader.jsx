3d8d1fb3de097fff77b73140b5ff0df1181bbef9
mapyankov <mapyankov@gmail.com>
2023-03-26 21:50:54 +0300

    2commit

---
+++ src/componets/preloader.jsx
@@ -0,0 +1,7 @@
+function Preloader() {
+    return <div className="progress purple lighten-5">
+        <div className="indeterminate deep-purple accent-1"></div>
+    </div>
+}
+
+export { Preloader };
